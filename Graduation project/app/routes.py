4a971a47993b884e7391fc80e46241d8e7f87fd1
VadymLinyvyi <raven.lazy@gmail.com>
2020-06-03 20:44:27 +0300

    login/logout services

--- Graduation project/app/routes.py
+++ Graduation project/app/routes.py
@@ -4,6 +4,9 @@ from app import app
 from flask_nav import Nav
 from flask_nav.elements import Navbar, Subgroup, View
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
+
 
 nav = Nav(app)
 nav.register_element(
@@ -11,7 +14,7 @@ nav.register_element(
     Navbar(
         "menu_bar",
         View('Автосервіс "Роги і копита"', 'index'),
-        View('Авторизація', 'login'),
+        View('Ввійти', 'login'),
         Subgroup(
             'Про компанію',
             View('Історія', 'history'),
@@ -98,9 +101,20 @@ def reviews():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Невірне ім'я користувача чи пароль")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Авторизація', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
