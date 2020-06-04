fd50efd68733888282c35f1313f1275a812613a6
Vadym Linyvyi <raven.lazy@gmail.com>
2020-06-04 03:36:44 +0300

    backup

--- Graduation project/app/routes.py
+++ Graduation project/app/routes.py
@@ -82,10 +82,13 @@ def reviews():
         db.session.add(post)
         db.session.commit()
         flash('Дякуємо за Ваш відгук!')
-        return redirect(url_for('index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template("reviews.html", title='Відгуки', form=form,
-                           posts=posts)
+        return redirect(url_for('reviews'))
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('reviews', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('reviews', page=posts.prev_num) if posts.has_prev else None
+    return render_template('reviews.html', title='Відгуки', form=form, posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
