7f8f1526c7fc4201aea7a22ca8a0384a625b4a45
Vadym Linyvyi <raven.lazy@gmail.com>
2020-06-04 03:04:11 +0300

    backup

--- Graduation project/app/forms.py
+++ Graduation project/app/forms.py
@@ -1,7 +1,23 @@
-# -*- coding: utf-8 -*-
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Ім'я користувача", validators=[DataRequired()])
+    about_me = TextAreaField('Про мене', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Підтвердити')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError("Виберіть інше ім'я")
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +25,28 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField("Запам'ятати")
     submit = SubmitField('Ввійти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Ім'я користувача", validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Повторіть пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зареєструватись')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Виберіть, будь-ласка, інше ім'я")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Виберіть, будь-ласка, інший e-mail")
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Залиште відгук', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Підтвердити')
--- Graduation project/app/routes.py
+++ Graduation project/app/routes.py
@@ -1,45 +1,35 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect, url_for
-from app import app
-from flask_nav import Nav
-from flask_nav.elements import Navbar, Subgroup, View
-from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
-from app.models import User
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, PostForm, RegistrationForm, EditProfileForm
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from app.models import User, Post
+from datetime import datetime
 
 
-nav = Nav(app)
-nav.register_element(
-    "main_menu_bar",
-    Navbar(
-        "menu_bar",
-        View('Автосервіс "Роги і копита"', 'index'),
-        View('Ввійти', 'login'),
-        Subgroup(
-            'Про компанію',
-            View('Історія', 'history'),
-            View('Програма SOS – Sправжня Опіка Sервісу', 'sos_program'),
-            View('Вакансії', 'vacancies'),
-            View('Контакти', 'contacts'),
-            View('Відгуки', 'reviews')
-        ),
-        Subgroup(
-            'Автосервіс',
-            View('Записатись на сервіс', 'service'),
-            View('Кузовний ремонт', 'service'),
-            View('Запчастини', 'service'),
-            View('Аксесуари для автомобілів', 'service'),
-            View('Вартість технічного обслуговування', 'service'),
-            View('Програма лояльності', 'service'),
-            View('Корпоративним клієнтам', 'service')
-        ),
-        Subgroup(
-            'Прес-центр',
-            View('Новини', 'press'),
-            View('Акції', 'press')
-        )
-    )
-)
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Зміни збережено')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Редагувати профіль',
+                           form=form)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route('/')
@@ -83,20 +73,19 @@ def contacts():
     return 'pass'
 
 
-@app.route('/reviews')
+@app.route('/reviews', methods=['GET', 'POST'])
+@login_required
 def reviews():
-    user = {'username': 'User1'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('reviews.html', title='Відгуки', user=user, posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Дякуємо за Ваш відгук!')
+        return redirect(url_for('index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template("reviews.html", title='Відгуки', form=form,
+                           posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -107,14 +96,43 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash("Невірне ім'я користувача чи пароль")
+            flash("невірне ім'я користувача або пароль")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Авторизація', form=form)
 
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for("index"))
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
